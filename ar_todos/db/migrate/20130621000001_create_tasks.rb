fc952a6a38192aae5093a2a20db69470641c5699
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-21 11:47:35 -0500

    added migrations and create db

--- ar_todos/db/migrate/20130621000001_create_tasks.rb
+++ ar_todos/db/migrate/20130621000001_create_tasks.rb
@@ -1,11 +1,11 @@
-require_relative '../config'
+require_relative '../../config/application'
 
 class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |task|
-      list.string :description
-      list.integer  :completed
-      list.timestamps # Check this later
+      task.string :description
+      task.integer  :completed
+      task.timestamps # Check this later
     end
   end
 end
